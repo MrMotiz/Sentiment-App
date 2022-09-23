8a3888aa88f2a6e3993316a294108017018879dc
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2022-09-23 17:24:37 +0100

    Try to add external API

--- src/main/java/com/example/sentiment_app/service/TextService.java
+++ src/main/java/com/example/sentiment_app/service/TextService.java
@@ -1,7 +1,10 @@
 package com.example.sentiment_app.service;
 
+import com.example.sentiment_app.model.Text;
+import com.example.sentiment_app.repository.TextRepository;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,9 +14,10 @@ import java.net.http.HttpResponse;
 public class TextService {
 
 
+    Text text = new Text();
     private TextRepository textRepository;
 
-    public TextService(TextRepository textRepository) {
+    public TextService(TextRepository textRepository) throws IOException, InterruptedException {
         this.textRepository = textRepository;
     }
 
@@ -21,9 +25,17 @@ public class TextService {
     public TextDto createText(CreateTextDto textDto){
         Text text = TextConverter.convertCreateTExtDtoToEntity(textDto);
         text = textRepository.save(text);
-        HttpResponse<String> response = HttpClient.newHttpClient().send(textRepository.getMessage, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = HttpClient.newHttpClient().send(text.getMessage, HttpResponse.BodyHandlers.ofString());
         return TextConverter.convertTextDto(text, response);
 
     }
+    HttpRequest postRequest = HttpRequest.newBuilder()
+            .uri(URI.create("https://text-analysis12.p.rapidapi.com/article-extraction/api/v1.3"))
+            .header("X-RapidAPI-Key", "95bd6ffc51mshd854e2b69156aaap139628jsn391b570980b5")
+            .POST(HttpRequest.BodyPublishers.ofString(text.getMessage))
+            .build();
+    HttpClient httpsClient = HttpClient.newHttpClient();
+
+    HttpResponse<String> postResponse = httpsClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 
 }
