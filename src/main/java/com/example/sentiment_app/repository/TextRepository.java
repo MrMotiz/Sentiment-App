a87f7e0550dc755b2da9cf4a4d9392c25269d011
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2022-09-27 12:48:03 +0100

    Fixed connections

--- src/main/java/com/example/sentiment_app/repository/TextRepository.java
+++ src/main/java/com/example/sentiment_app/repository/TextRepository.java
@@ -1,7 +1,10 @@
 package com.example.sentiment_app.repository;
 
 import com.example.sentiment_app.model.Text;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface TextRepository extends JpaRepository<Text,Integer> {
 }
