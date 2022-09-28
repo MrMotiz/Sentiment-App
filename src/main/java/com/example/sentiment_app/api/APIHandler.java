d411b689dac5ba163563c60e0ee1aee6c81d72bc
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2022-09-28 11:39:55 +0100

    MVP ready

--- src/main/java/com/example/sentiment_app/api/APIHandler.java
+++ src/main/java/com/example/sentiment_app/api/APIHandler.java
@@ -2,6 +2,7 @@ package com.example.sentiment_app.api;
 
 import com.example.sentiment_app.command.TextDto;
 import com.example.sentiment_app.model.Text;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,17 +22,39 @@ public class APIHandler {
 
     public Text APIHandler(String message) throws IOException, InterruptedException, JSONException {
 
-        JSONObject myJsonObj = new JSONObject(api.postRequest(message));
 
-        text.setSentiment(myJsonObj.getString("sentiment"));
+        JSONObject saveSentiment = new JSONObject(api.postRequest(message));
 
-        String sentimentList = myJsonObj.getString("aggregate_sentiment");
+        text.setSentiment(saveSentiment.getString("sentiment"));
 
-        JSONObject aggregate = new JSONObject(sentimentList);
+        JSONArray newObject= new JSONArray(saveSentiment.getString("sentiment_list"));
+        System.out.println("_________________");
+        System.out.println(newObject.getString(0));
+        System.out.println("_________________");
+
+
+        JSONObject newObject2= new JSONObject(newObject.getString(0));
+
+        System.out.println("_________________");
+        System.out.println(newObject2.getString("sentence"));
+        System.out.println("_________________");
+
+        for (String v: newObject2.toString().split(",")) {
+            if(v.contains("sentence=")){
+                text.setSentence(v.substring(10));
+                break;
+            }
+            System.out.println(v);
+        }
+
+        JSONObject saveTextAPIInfo = new JSONObject(api.postRequest(message));
+
+        JSONObject aggregate = new JSONObject(saveTextAPIInfo.getString("aggregate_sentiment"));
 
         text.setNeg(Double.parseDouble(aggregate.getString("neg")));
         text.setNeu(Double.parseDouble(aggregate.getString("neu")));
         text.setPos(Double.parseDouble(aggregate.getString("pos")));
+        //text.setSentence(aggregate.getString("sentence"));
 
         return text;
 
