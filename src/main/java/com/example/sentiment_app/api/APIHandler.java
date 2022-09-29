920a1dfec88106c79f27945064d03cc7e63093b9
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2022-09-29 10:35:40 +0100

    FINAL COMMIT. MVP READY

--- src/main/java/com/example/sentiment_app/api/APIHandler.java
+++ src/main/java/com/example/sentiment_app/api/APIHandler.java
@@ -1,7 +1,6 @@
 package com.example.sentiment_app.api;
 
 import com.example.sentiment_app.model.Text;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +17,6 @@ public class APIHandler {
         JSONObject APIInfo = new JSONObject(api.postRequest(text));
 
         text.setSentiment(APIInfo.getString("sentiment"));
-
-        JSONArray newObject= new JSONArray(APIInfo.getString("sentiment_list"));
-        System.out.println(newObject);
-        for (String v: newObject.toString().split(",")) {
-            if(v.contains("sentence")){
-                System.out.println(v);
-                text.setSentence(v.substring(14));
-                break;
-            }
-        }
 
         JSONObject aggregate = new JSONObject(APIInfo.getString("aggregate_sentiment"));
 
